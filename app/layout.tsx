00b6244e23724a46a1668b70e3df8afbce01d3d2
lhabib lachil <habib.lachil@gmail.com>
2026-03-21 21:09:21 +0000

    add google tag to site

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-KHMRSWP5');`}
+          })(window,document,'script','dataLayer','GTM-KWTWMTFP');`}
         </Script>
       </head>
       <body
@@ -39,7 +39,7 @@ export default function RootLayout({
       >
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KHMRSWP5"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KWTWMTFP"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
