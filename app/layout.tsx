9377f1e4ef5652f0d7cda658d52d64f7786f5c8b
lhabib lachil <habib.lachil@gmail.com>
2026-07-05 15:26:47 +0200

    hotjar entfernen

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,22 +33,7 @@ export default function RootLayout({
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-KWTWMTFP');`}
         </Script>
-        <Script
-        id="hotjar"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:6422631,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              if (a && a.appendChild) a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          `,
-        }}
-      />
+        
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
